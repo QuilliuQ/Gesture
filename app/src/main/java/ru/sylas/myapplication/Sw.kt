36ba53fb8acffe5df64c59844bf50a2752c9302b
sylas <sylas>
2022-03-30 22:10:46 +0500

    lol

--- app/src/main/java/ru/sylas/myapplication/Sw.kt
+++ app/src/main/java/ru/sylas/myapplication/Sw.kt
@@ -1,12 +1,14 @@
 package ru.sylas.myapplication
 
 import android.content.Context
+import android.util.Log
 import android.view.GestureDetector
 import android.view.GestureDetector.SimpleOnGestureListener
 import android.view.MotionEvent
 import android.view.View
 import android.view.View.OnTouchListener
 import kotlin.math.abs
+import kotlin.math.log
 
 
 open class OnSwipeTouchListener(ctx: Context?) : OnTouchListener {
@@ -20,6 +22,7 @@ open class OnSwipeTouchListener(ctx: Context?) : OnTouchListener {
             return true
         }
 
+        //Fling типо свайпа мы его прослушаивам
         override fun onFling(
             e1: MotionEvent,
             e2: MotionEvent,
@@ -28,10 +31,22 @@ open class OnSwipeTouchListener(ctx: Context?) : OnTouchListener {
         ): Boolean {
             var result = false
             try {
+                /*
+                * e1 действие начала
+                * е2 дейтсвие конца
+                * ну или положение нашего пальца чтобы было понятнее
+                * для начала нам нужно найти разницу между первый и вторым положением, надо нам это для того чтобы отследить именно свайп
+                * */
                 val diffY = e2.y - e1.y
                 val diffX = e2.x - e1.x
+                Log.d("My", "$diffX  $diffY")
+                // дальше мы сравниваем эти разницы для того чтобы понять на думать о свайпе влево/вправо или вверх/вниз
                 if (abs(diffX) > abs(diffY)) {
+                    //дальше идет сравнение разницы сначала расстояния тоесть свайп будет считать если ты пальцем проведешь больше 100 единиц по x
+                        // и также сравнивается скорость так как свайп действие быстрого смахивания если соответствует то идем дальше
                     if (abs(diffX) > Companion.SWIPE_THRESHOLD && abs(velocityX) > Companion.SWIPE_VELOCITY_THRESHOLD) {
+//                        ну здесь элементарно проверяется в какую сторону произошел свайп так как по системе координат х увеличивается вправо
+
                         if (diffX > 0) {
                             onSwipeRight()
                         } else {
@@ -40,6 +55,7 @@ open class OnSwipeTouchListener(ctx: Context?) : OnTouchListener {
                         result = true
                     }
                 } else if (abs(diffY) > Companion.SWIPE_THRESHOLD && abs(velocityY) > Companion.SWIPE_VELOCITY_THRESHOLD) {
+                    // казалось бы а почему, тогда здесь наоборот. А вот незнаю почемуто увеличение по y идет зеркально стандартной оси координат
                     if (diffY > 0) {
                         onSwipeBottom()
                     } else {
@@ -47,6 +63,7 @@ open class OnSwipeTouchListener(ctx: Context?) : OnTouchListener {
                     }
                     result = true
                 }
+
             } catch (exception: Exception) {
                 exception.printStackTrace()
             }
